2f3f0fca15d578ab446e68c57c881d3a6c7cf702
Aleksey Trubitsyn <at.8441451@yandex.ru>
2019-06-16 20:08:00 +0300

    create CatalogGrid component

---
+++ src/components/catalog-grid/catalog-grid.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import CatalogItem, { catalogItemPropTypes } from '../catalog-item';
+
+const propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape(
+      catalogItemPropTypes
+    ).isRequired
+  ).isRequired,
+  className: PropTypes.string,
+  listMode: PropTypes.bool,
+};
+
+const defaultProps = {
+  className: '',
+  listMode: false,
+};
+
+const CatalogGrid = (props) => {
+  const {
+    className,
+    listMode,
+    items
+  } = props;
+
+  return (
+    <div className={`${className} catalog-grid ${listMode ? 'catalog-grid_list-mode' : ''}`.trim()}>
+      {items.map(item => (
+        <div
+          className="catalog-grid__item catalog-grid-item"
+          key={item.id}
+        >
+          <CatalogItem
+            {...item}
+            className="catalog-grid-item__content"
+            listMode={listMode}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+CatalogGrid.propTypes = propTypes;
+CatalogGrid.defaultProps = defaultProps;
+export default CatalogGrid;

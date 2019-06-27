96bae0eb74786ae3f1ad2d26a6e25367186101b4
Aleksey Trubitsyn <at.8441451@yandex.ru>
2019-06-27 19:27:13 +0300

    create reducer with tests for 'CLONE_LAST_BEFORE_FIRST' action

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,8 @@
-import * as types from '../constants/actionTypes';
+import _cloneDeep from 'lodash/cloneDeep';
+
+import {
+  CLONE_LAST_BEFORE_FIRST
+} from '../constants/actionTypes';
 
 import { data } from '../data.json';
 
@@ -8,7 +12,33 @@ const initialState = {
 };
 
 export default (state = initialState, action) => {
+  const cloneLastBeforeFirst = (items, nextId) => {
+    if (!items.length) {
+      return {
+        items,
+        nextId
+      };
+    }
+
+    const itemToClone = items[items.length - 1];
+    const clonedItem = {
+      ..._cloneDeep(itemToClone),
+      id: nextId
+    };
+
+    return {
+      items: [clonedItem, ...items],
+      nextId: nextId + 1
+    };
+  };
+
   switch (action.type) {
+    case CLONE_LAST_BEFORE_FIRST:
+      return {
+        ...state,
+        ...cloneLastBeforeFirst(state.items, state.nextId)
+      };
+
     default:
       return state;
   }

a4cc200041abbe1a33e6de7b4ffcf91836c07bbd
Aleksey Trubitsyn <at.8441451@yandex.ru>
2019-06-28 15:36:27 +0300

    CatalogActions: disable action buttons if items array is empty

--- src/components/catalog-actions/catalog-actions.jsx
+++ src/components/catalog-actions/catalog-actions.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const propTypes = {
+  arrayIsEmpty: PropTypes.bool.isRequired,
   onCloneLastToFirst: PropTypes.func.isRequired,
   onCloneFirstToLast: PropTypes.func.isRequired,
   onRemoveFirst: PropTypes.func.isRequired,
@@ -11,42 +12,61 @@ const propTypes = {
 const CatalogActions = (props) => {
   const {
     className,
+    arrayIsEmpty,
     onCloneLastToFirst,
     onCloneFirstToLast,
     onRemoveFirst,
     onRemoveLast
   } = props;
 
+  const emptyArrayError = 'Действие недоступно для пустого списка. Создайте элемент на форме внизу страницы';
+
   return (
     <div className={`${className} catalog-actions`.trim()}>
       <button
         className="btn btn-primary btn-inline catalog-actions__button"
-        title="Скопировать последний элемент и вставить в начало списка"
+        title={`${arrayIsEmpty
+          ? emptyArrayError
+          : 'Скопировать последний элемент и вставить в начало списка'}
+        `}
         type="button"
+        disabled={arrayIsEmpty}
         onClick={onCloneLastToFirst}
       >
         {'Добавить в начало'}
       </button>
       <button
         className="btn btn-primary btn-inline catalog-actions__button"
-        title="Скопировать первый элемент и вставить в конец списка"
+        title={`${arrayIsEmpty
+          ? emptyArrayError
+          : 'Скопировать первый элемент и вставить в конец списка'
+        }`}
         type="button"
+        disabled={arrayIsEmpty}
         onClick={onCloneFirstToLast}
       >
         {'Добавить в конец'}
       </button>
       <button
         className="btn btn-primary btn-inline catalog-actions__button"
-        title="Удалить первый элемент"
+        title={`${arrayIsEmpty
+          ? emptyArrayError
+          : 'Удалить первый элемент'
+        }`}
         type="button"
+        disabled={arrayIsEmpty}
         onClick={onRemoveFirst}
       >
         {'Удалить первый'}
       </button>
       <button
         className="btn btn-primary btn-inline catalog-actions__button"
-        title="Удалить последний элемент"
+        title={`${arrayIsEmpty
+          ? emptyArrayError
+          : 'Удалить последний элемент'
+        }`}
         type="button"
+        disabled={arrayIsEmpty}
         onClick={onRemoveLast}
       >
         {'Удалить последний'}

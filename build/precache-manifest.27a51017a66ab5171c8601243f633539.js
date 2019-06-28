354914029b46be2183d7f4f4a9e376b797518036
Aleksey Trubitsyn <at.8441451@yandex.ru>
2019-06-28 15:41:00 +0300

    build

---
+++ build/precache-manifest.27a51017a66ab5171c8601243f633539.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a37407eb260e3e2a1f23bf9c604e54e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8b8b641a27879903e435",
+    "url": "./static/css/main.4693be1a.chunk.css"
+  },
+  {
+    "revision": "e3914b81126f800dd69b",
+    "url": "./static/js/2.00b402bc.chunk.js"
+  },
+  {
+    "revision": "8b8b641a27879903e435",
+    "url": "./static/js/main.5c9687d7.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
